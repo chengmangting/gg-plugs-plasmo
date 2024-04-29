e4be040c4c2f109f796aef6946a30760af468429
chenmanting <chenmanting>
2024-04-29 17:35:16 +0800

    fix：初始化

--- popup.vue
+++ popup.vue
@@ -1,61 +1,16 @@
 <template>
-  <div>
-    <h2 class="text-center">
-      Welcome to your
-      <a href="https://www.plasmo.com" target="_blank">Plasmo</a> Extension!
-    </h2>
-
-    <div class="container">
-      <button @click="decrement">-</button>
-      <p>
-        <b>{{ state.count }}</b>
-      </p>
-      <button @click="increment">+</button>
-    </div>
+  <div class="popup_page">
+    安装后请刷新浏览器使用此插件
+    <div class="popup_page_main"></div>
   </div>
-  <p v-if="state.action" class="action text-center">
-    {{ state.action }}
-  </p>
-  <a href="https://docs.plasmo.com" target="_blank"> View Docs </a>
 </template>
-
-<style>
-.container {
-  min-width: 470px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 47px;
-}
-.text-center {
-  text-align: center;
-}
-.action {
-  color: #470;
-  font-weight: bold;
+<style lang="less" scoped>
+.popup_page {
+  width: 240px;
+  color: #000;
+  font-size: 16px;
+  .popup_page_main {
+    color: green;
+  }
 }
 </style>
-
-<script setup lang="ts">
-import { reactive } from "vue"
-import type { App } from "vue"
-
-const state = reactive({ count: 0, action: null })
-
-function increment() {
-  state.count++
-  state.action = "increment"
-}
-
-function decrement() {
-  state.count--
-  state.action = "decrement"
-}
-
-defineOptions({
-  prepare(app: App) {
-    // Use any plugins here:
-    // app.use
-  }
-})
-</script>
